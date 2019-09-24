037934663866219468f84aaebab89476f70b9a31
CalexODE <alexandre.alvesandrade@hotmail.com>
2019-09-24 15:21:37 -0300

    First commit

---
+++ include/node.hpp
@@ -0,0 +1,15 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <string>
+using namespace std;
+
+class Node
+{
+	public:
+		string valor;
+		Node* Lchild;
+		Node* Rchild;
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "../include/node.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Node* no = new Node();
+	no->valor = "Alexandre";
+
+	return 0;
+}
